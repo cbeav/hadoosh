9ce1354c1de8f470544357d1271e89d65743ad04
Chris Beavers <crbeavers@gmail.com>
2012-11-16 11:55:49 -0800

    Added support for piping to local commands. Currently only goes one level deep, but will generalize soon

--- HadooSh.java
+++ HadooSh.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -31,317 +32,313 @@ public class HadooSh {
 	static String rootStr;
 	static Path root;
 
+	private final static boolean DEBUG = false;
+
 	public static void main(String[] args) throws Exception {
 		config = new Configuration();
-		//config.set("fs.default.name", "hdfs://localhost:9000");
+		// config.set("fs.default.name", "hdfs://localhost:9000");
 		fs = FileSystem.get(config);
-		
+
 		p = fs.getWorkingDirectory();
 		home = new Path(p.toString());
 
 		String homeStr = home.toString();
 		rootStr = homeStr.substring(0, homeStr.indexOf("/user/", 0));
 		root = new Path(rootStr);
-		
+
 		ConsoleReader reader = new ConsoleReader();
 		reader.setBellEnabled(false);
 		List completors = new LinkedList();
-		String[] commandsList = new String[] {"cd", "ls", "pwd", "exit", "cat", "head", "local"};
+		String[] commandsList = new String[] { "cd", "ls", "pwd", "exit",
+				"cat", "head", "local" };
 		Completor fileCompletor = new HDFSCompletor();
 		completors.add(new SimpleCompletor(commandsList));
 		completors.add(fileCompletor);
 		reader.addCompletor(new ArgumentCompletor(completors));
-	
-		
+
 		PrintWriter out = new PrintWriter(System.out);
 		String line;
-		
-		while ((line = reader.readLine(trimToLeaf(p.toString()) + " > ").trim()) != null)
-		{
-			PipedInputStream is;
-			if (line.equals("exit"))
-				break;
-			else
-			{
-				is = getCmdOutput(line);
+
+		while ((line = reader.readLine(trimToLeaf(p.toString()) + " > ").trim()) != null) {
+			try {
+				PipedInputStream is;
+				if (line.equals("exit"))
+					break;
+				else {
+					String[] pipeBreaks = line.split("\\|");
+					// For now assume command is on hdfs
+					is = getCmdOutput(pipeBreaks[0]);
+					if (pipeBreaks.length > 1) {
+						Runtime rt = java.lang.Runtime.getRuntime();
+						Process p = rt.exec(pipeBreaks[1] + " -");
+						OutputStream os = p.getOutputStream();
+						dumpToOS(is, os);
+						os.close();
+						InputStream newIn = p.getInputStream();
+						dump(newIn, System.out);
+					} else
+						dump(is, System.out);
+
+				}
+				out.flush();
+			} catch (Exception e) {
+				if (DEBUG)
+					e.printStackTrace();
+				System.out.println("command not found");
+			} catch (Throwable e) {
+				if (DEBUG)
+					e.printStackTrace();
+				System.out.println();
 			}
-			pipe("", is);
-			out.flush();
 		}
 	}
-	
-	private static PipedInputStream pipe(String cmd, PipedInputStream pis) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(pis));
+
+	private static void dumpToOS(InputStream is, OutputStream os)
+			throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		String line;
-		while((line = br.readLine()) != null)
-			System.out.println(line);
-		
+		while ((line = br.readLine()) != null)
+		{
+			os.write(line.getBytes());
+			os.write('\n');
+		}
+
 		br.close();
-		pis.close();
-		
+		is.close();
+	}
+	
+	private static void dump(InputStream is, PrintStream os)
+			throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line;
+		while ((line = br.readLine()) != null)
+		{
+			os.write(line.getBytes());
+			os.write('\n');
+		}
+
+		br.close();
+		is.close();
+	}
+
+	private static PipedInputStream getCmdOutput(String cmd)
+			throws IOException, InterruptedException {
+
+		PipedInputStream pis = new PipedInputStream(10000);
+
+		String line = cmd;
+
+		PipedOutputStream os = new PipedOutputStream();
+		os.connect(pis);
+
+		if (line.startsWith("ls"))
+			ls(line, os);
+		else if (line.startsWith("cd"))
+			cd(line, os);
+		else if (line.startsWith("cat"))
+			cat(line, os);
+		else if (line.startsWith("head"))
+			head(line, os);
+		else if (line.startsWith("pwd"))
+			pwd(line, os);
+		else if (line.startsWith("local"))
+			sysExec(line.substring(line.indexOf("local") + "local".length()),
+					os);
+		else
+			sysExec(line, os);
+
+		os.close();
 		return pis;
 	}
-	
-	private static PipedInputStream getCmdOutput(String cmd)
-	{
 
-		PipedInputStream pis = new PipedInputStream();
-		try
-		{
-			String[] pipeBreaks = cmd.split("\\|");
-			String line = pipeBreaks[0];
-			
-			
-			PipedOutputStream os = new PipedOutputStream();
-			os.connect(pis);
-			
-			if(line.startsWith("ls"))
-				ls(line, os);
-			else if(line.startsWith("cd"))
-				cd(line, os);
-			else if(line.startsWith("cat"))
-				cat(line, os);
-			else if(line.startsWith("head"))
-				head(line, os);
-			else if(line.startsWith("pwd"))
-				pwd(line, os);
-			else if(line.startsWith("local"))
-				sysExec(line.substring(line.indexOf("local") + "local".length()), os);
-			else
-				sysExec(line, os);
-			
-			os.flush();
-			os.close();
-			return pis;
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			// Do nothing!
-		}
-		catch (Throwable t)
-		{
-			t.printStackTrace();
-			// Do nothing!
-		}
-		return null;
-	}
-	
-	private static void println(OutputStream os, String s) throws IOException
-	{
-		os.write((s + "\n").getBytes());
-	}
-	
-	private static void sysExec(String line, OutputStream os) throws IOException, InterruptedException {
-        Runtime rt = java.lang.Runtime.getRuntime();
-        Process p = rt.exec(line);
-        p.waitFor();
-        InputStream is = p.getInputStream();
-        InputStream es = p.getErrorStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        
-        String s = null;
-        while ((s = br.readLine()) != null) {
-            println(os, s);
-        }
-        br.close();
-        is.close();
-
-        br = new BufferedReader(new InputStreamReader(es));
-        s = null;
-        while ((s = br.readLine()) != null) {
-            println(os, s);
-        }
-        br.close();
-        es.close();
-	}
-	
-	private static void pwd(String line, OutputStream os) throws UnsupportedEncodingException, IOException
-	{
-		println(os, p.toString().substring(p.toString().indexOf(rootStr) + rootStr.length()));
+	private static void println(OutputStream os, String s) throws IOException {
+		os.write(s.getBytes());
+		os.write('\n');
+		os.flush();
 	}
 
-	
-	public static void ls(String fullCommand, OutputStream os) throws IOException
-	{
+	private static void sysExec(String line, OutputStream os)
+			throws IOException, InterruptedException {
+		Runtime rt = java.lang.Runtime.getRuntime();
+		Process p = rt.exec(line);
+		p.waitFor();
+		InputStream is = p.getInputStream();
+		InputStream es = p.getErrorStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		String s = null;
+		while ((s = br.readLine()) != null) {
+			println(os, s);
+		}
+		br.close();
+		is.close();
+
+		br = new BufferedReader(new InputStreamReader(es));
+		s = null;
+		while ((s = br.readLine()) != null) {
+			println(os, s);
+		}
+		br.close();
+		es.close();
+	}
+
+	private static void pwd(String line, OutputStream os)
+			throws UnsupportedEncodingException, IOException {
+		println(os,
+				p.toString().substring(
+						p.toString().indexOf(rootStr) + rootStr.length()));
+	}
+
+	public static void ls(String fullCommand, OutputStream os)
+			throws IOException {
 		String[] parts = fullCommand.split(" ");
 		Path targetDir = null;
-		if(parts.length > 2)
-		{
+		if (parts.length > 2) {
 			println(os, "You can't change to multiple directories");
 			return;
-		}
-		else if(parts.length == 1)
+		} else if (parts.length == 1)
 			targetDir = p;
-		else
-		{
+		else {
 			targetDir = new Path(parts[1]);
 		}
-		
+
 		FileStatus[] stati = fs.listStatus(targetDir);
-		for(FileStatus f : stati)
-		{
+		for (FileStatus f : stati) {
 			String s = trimToLeaf(f.getPath().toString());
 			println(os, f.isDir() ? s + "/" : s);
 		}
 	}
-	
-	public static void cat(String fullCommand, OutputStream os) throws IOException
-	{
+
+	public static void cat(String fullCommand, OutputStream os)
+			throws IOException {
 		String[] parts = fullCommand.split(" ");
-		if(parts.length == 1)
-		{
+		if (parts.length == 1) {
 			println(os, "Error: not a file");
 			return;
-		}
-		else
-		{
-			for(int i=1; i<parts.length; i++)
-			{
+		} else {
+			for (int i = 1; i < parts.length; i++) {
 				Path f = new Path(p.toString(), parts[i]);
-				if(fs.exists(f))
-				{
-					BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(f)));
+				if (fs.exists(f)) {
+					BufferedReader br = new BufferedReader(
+							new InputStreamReader(fs.open(f)));
 					String s;
-				    while((s=br.readLine()) != null)
-					{
-					    println(os, s);
+					while ((s = br.readLine()) != null) {
+						println(os, s);
 					}
-				    br.close();
-				}
-				else
-				{
+					br.close();
+				} else {
 					println(os, "Error: not a file");
 				}
 			}
 		}
 	}
-	
-	public static void head(String fullCommand, OutputStream os) throws IOException
-	{
+
+	public static void head(String fullCommand, OutputStream os)
+			throws IOException {
 		String[] parts = fullCommand.split(" ");
-		if(parts.length > 3)
-		{
+		if (parts.length > 3) {
 			println(os, "Usage error: head [numLines] file");
 			return;
-		}
-		else if(parts.length == 1)
-		{
+		} else if (parts.length == 1) {
 			println(os, "Error: not a file");
 			return;
-		}
-		else
-		{
-			try
-			{
+		} else {
+			try {
 				int numLines = 1;
 				Path f;
-				if(parts.length == 3)
-				{
+				if (parts.length == 3) {
 					numLines = Integer.parseInt(parts[1]);
 					f = new Path(p.toString(), parts[2]);
-				}
-				else
+				} else
 					f = new Path(p.toString(), parts[1]);
-				if(fs.exists(f))
-				{
-					BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(f)));
+				if (fs.exists(f)) {
+					BufferedReader br = new BufferedReader(
+							new InputStreamReader(fs.open(f)));
 					String line = null;
 					int count = 0;
-					while((line = br.readLine()) != null && count < numLines)
-					{
+					while ((line = br.readLine()) != null && count < numLines) {
 						println(os, line);
 						count++;
 					}
 					br.close();
-				}
-				else
-				{
+				} else {
 					println(os, "Error: no such file");
 				}
-			} catch (Exception e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
 				println(os, "Usage error: head [numLines] file");
 			}
 		}
 	}
-	
-	public static void cd(String fullCommand, OutputStream os) throws IOException
-	{
+
+	public static void cd(String fullCommand, OutputStream os)
+			throws IOException {
 		String[] parts = fullCommand.split(" ");
-		if(parts.length > 2)
+		if (parts.length > 2)
 			println(os, "You can't change to multiple directories");
-		else if(parts.length == 1)
+		else if (parts.length == 1)
 			p = new Path(home.toString());
-		else if(parts[1].startsWith("/"))
-		{
+		else if (parts[1].startsWith("/")) {
 			Path targetPath = new Path(root, parts[1]);
-			if(fs.exists(targetPath))
+			if (fs.exists(targetPath))
 				p = targetPath;
 			else
 				println(os, "Path " + targetPath.toString() + " does not exist");
-		}
-		else
-		{
+		} else {
 			Path targetPath = new Path(p, parts[1]);
-			if(fs.exists(targetPath))
+			if (fs.exists(targetPath))
 				p = targetPath;
 			else
 				println(os, "Path " + targetPath.toString() + " does not exist");
 		}
 	}
-	
-	public static String trimToLeaf(String path)
-	{
+
+	public static String trimToLeaf(String path) {
 		String[] parts = path.split("/");
 		return parts[parts.length - 1];
 	}
-	
-	private static class HDFSCompletor implements Completor
-	{
-		private SimpleCompletorWithoutSpace completor; 
-		
-		public HDFSCompletor()
-		{
+
+	private static class HDFSCompletor implements Completor {
+		private SimpleCompletorWithoutSpace completor;
+
+		public HDFSCompletor() {
 			completor = new SimpleCompletorWithoutSpace(new String[] {});
 		}
-		
-		public int complete(final String buffer, final int cursor, final List clist) {
+
+		public int complete(final String buffer, final int cursor,
+				final List clist) {
 			String myBuffer = buffer == null ? "" : buffer;
 			int lastSlash = myBuffer.lastIndexOf('/');
 			String pathDir, pathCont;
 			try {
 				pathDir = myBuffer.substring(0, lastSlash);
-				pathCont = myBuffer.substring(lastSlash+1, myBuffer.length());
+				pathCont = myBuffer.substring(lastSlash + 1, myBuffer.length());
 			} catch (Exception e) {
 				pathDir = "";
 				pathCont = myBuffer;
 			}
-			
+
 			String dir;
-			if(myBuffer.startsWith("/"))
-			{
+			if (myBuffer.startsWith("/")) {
 				dir = rootStr + "/";
 			} else {
 				dir = p.toString() + "/";
 			}
-			
+
 			if (!pathDir.equals("") || myBuffer.equals("/")) {
 				String extra = pathDir;
 				if (myBuffer.startsWith("/"))
 					extra = extra.substring(1);
 				dir += extra + "/";
 			}
-			
+
 			String prefix = dir;
 			if (!pathCont.equals("")) {
 				prefix += pathCont;
 			}
-			
+
 			PathFilter pf = new MatchingPrefixPathFilter(prefix);
-			
+
 			FileStatus[] completions;
 			try {
 				completions = fs.listStatus(new Path(dir), pf);
@@ -350,30 +347,28 @@ public class HadooSh {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			String[] candidates = new String[completions.length];
-			
+
 			for (int i = 0; i < completions.length; i++) {
 				candidates[i] = trimToLeaf(completions[i].getPath().toString());
 				if (!pathDir.equals("")) {
 					candidates[i] = pathDir + "/" + candidates[i];
-				}
-				else if (myBuffer.startsWith("/")) {
+				} else if (myBuffer.startsWith("/")) {
 					candidates[i] = "/" + candidates[i];
 				}
-				
-				if(completions[i].isDir())
+
+				if (completions[i].isDir())
 					candidates[i] += "/";
 				else
 					candidates[i] += " ";
 			}
-			
+
 			completor.setCandidateStrings(candidates);
 			return completor.complete(myBuffer, cursor, clist);
 		}
 	}
 
-	
 	private static class MatchingPrefixPathFilter implements PathFilter {
 		String _prefix;
 
